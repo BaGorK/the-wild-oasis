26141892b27599349bbbe103f87a7b3a3338dc32
BaGorK <edmealemkassahun@gmail.com>
2024-03-02 06:18:33 +0300

    feat: using react-icons in main-navigation

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,5 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+import {
+  HiOutlineHome,
+  HiOutlineHomeModern,
+  HiOutlineUsers,
+  HiOutlineCog6Tooth,
+  HiOutlineCalendarDays,
+} from 'react-icons/hi2';
 
 const NavList = styled.ul`
   display: flex;
@@ -51,19 +58,34 @@ function MainNav() {
     <nav>
       <NavList>
         <li>
-          <StyledNavLink to='dashboard'>Home</StyledNavLink>
+          <StyledNavLink to='dashboard'>
+            <HiOutlineHome />
+            <span>Home</span>
+          </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to='bookings'>Bookings</StyledNavLink>
+          <StyledNavLink to='bookings'>
+            <HiOutlineCalendarDays />
+            <span>Bookings</span>
+          </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to='cabins'>Cabins</StyledNavLink>
+          <StyledNavLink to='cabins'>
+            <HiOutlineHomeModern />
+            <span>Cabins</span>
+          </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to='/users'>Users</StyledNavLink>
+          <StyledNavLink to='/users'>
+            <HiOutlineUsers />
+            <span>Users</span>
+          </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to='Settings'>Settings</StyledNavLink>
+          <StyledNavLink to='settings'>
+            <HiOutlineCog6Tooth />
+            <span>Settings</span>
+          </StyledNavLink>
         </li>
       </NavList>
     </nav>
