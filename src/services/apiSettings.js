1c31e812a82707ad3d482f549d12c0af67d7ec2f
BaGorK <edmealemkassahun@gmail.com>
2024-03-14 21:25:18 +0300

    feat: fetching applications settings from supabase

--- src/services/apiSettings.js
+++ src/services/apiSettings.js
@@ -1,11 +1,11 @@
-import supabase from "./supabase";
+import supabase from './supabase';
 
 export async function getSettings() {
-  const { data, error } = await supabase.from("settings").select("*").single();
+  const { data, error } = await supabase.from('settings').select('*').single();
 
   if (error) {
     console.error(error);
-    throw new Error("Settings could not be loaded");
+    throw new Error('Settings could not be loaded');
   }
   return data;
 }
@@ -13,15 +13,15 @@ export async function getSettings() {
 // We expect a newSetting object that looks like {setting: newValue}
 export async function updateSetting(newSetting) {
   const { data, error } = await supabase
-    .from("settings")
+    .from('settings')
     .update(newSetting)
     // There is only ONE row of settings, and it has the ID=1, and so this is the updated one
-    .eq("id", 1)
+    .eq('id', 1)
     .single();
 
   if (error) {
     console.error(error);
-    throw new Error("Settings could not be updated");
+    throw new Error('Settings could not be updated');
   }
   return data;
 }
