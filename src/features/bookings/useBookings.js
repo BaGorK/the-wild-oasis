98cbaaf4bd8830bd8b082f5693cb4db956a225d3
BaGorK <edmealemkassahun@gmail.com>
2024-03-26 13:17:49 +0300

    feat: filtering bookings | api side filtering

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,25 @@
+import { useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
+
 import { getBookings } from '../../services/apiBookings';
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filterValue = searchParams.get('status');
+  const filter =
+    !filterValue || filterValue === 'all'
+      ? null
+      : { field: 'status', value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
     queryKey: ['bookings'],
-    queryFn: getBookings,
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,10 +1,17 @@
 import { getToday } from '../utils/helpers';
 import supabase from './supabase';
 
-export async function getBookings() {
-  const { data, error } = await supabase
+export async function getBookings({ filter, sortBy }) {
+  let query = supabase
     .from('bookings')
     .select('*, cabins(name), guests(fullName, email)');
+
+  // FILTER
+  if (filter) {
+    query = query.eq(filter.field, filter.value);
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     console.error(error);
