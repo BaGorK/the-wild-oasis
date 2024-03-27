e01d00c4685f81244bef1f2fe9065b5899ab58e7
BaGorK <edmealemkassahun@gmail.com>
2024-03-28 02:02:00 +0300

    feat: prefetching with react query | fetching the next bookings page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,12 @@
 import { useSearchParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { getBookings } from '../../services/apiBookings';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+  const queryClient = useQueryClient();
+
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -23,6 +26,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+  // QUERY
   const {
     isLoading,
     data = {},
@@ -33,6 +37,20 @@ export function useBookings() {
   });
 
   const { data: bookings, count } = data;
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () =>
+        getBookings({
+          filter,
+          sortBy,
+          page: page + 1,
+        }),
+    });
 
   return { isLoading, bookings, error, count };
 }
