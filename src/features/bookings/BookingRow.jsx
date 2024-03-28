fe497a4499f3d14bbef63704875e1aefa7901837
BaGorK <edmealemkassahun@gmail.com>
2024-03-28 03:42:51 +0300

    feat: create a bookings detail page

--- src/features/bookings/BookingRow.jsx
+++ src/features/bookings/BookingRow.jsx
@@ -9,6 +9,7 @@ import { formatCurrency } from '../../utils/helpers';
 import { formatDistanceFromNow } from '../../utils/helpers';
 import Menus from '../../ui/Menus';
 import { HiEye } from 'react-icons/hi2';
+import { useNavigate } from 'react-router-dom';
 
 const Cabin = styled.div`
   font-size: 1.6rem;
@@ -51,6 +52,8 @@ function BookingRow({
     cabins: { name: cabinName },
   },
 }) {
+  const navigate = useNavigate();
+
   const statusToTagName = {
     unconfirmed: 'blue',
     'checked-in': 'green',
@@ -87,7 +90,12 @@ function BookingRow({
       <Menus.Menu>
         <Menus.Toggle id={bookingId} />
         <Menus.List id={bookingId}>
-          <Menus.Button icon={<HiEye />}>See details</Menus.Button>
+          <Menus.Button
+            icon={<HiEye />}
+            onClick={() => navigate(`/bookings/${bookingId}`)}
+          >
+            See details
+          </Menus.Button>
         </Menus.List>
       </Menus.Menu>
     </Table.Row>
---
+++ src/pages/Booking.jsx
@@ -0,0 +1,5 @@
+function Booking() {
+  return <div>booking detail page</div>;
+}
+
+export default Booking;
