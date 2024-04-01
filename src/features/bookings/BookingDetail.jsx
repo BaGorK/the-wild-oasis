18655e89e49d40bba43da8459ffb36febdfae1eb
BaGorK <edmealemkassahun@gmail.com>
2024-04-01 15:37:17 +0300

    fix: deleting a booking | navigating back to /booking from the details page

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -81,7 +81,11 @@ function BookingDetail() {
             <ConfirmDelete
               resourceName='booking'
               disabled={isDeleting}
-              onConfirm={() => deleteBooking(bookingId)}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
             />
           </Modal.Window>
         </Modal>
