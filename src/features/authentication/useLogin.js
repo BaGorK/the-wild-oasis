904fb2b3a5bd3a738e5be31b3e36a9eb8c34795e
BaGorK <edmealemkassahun@gmail.com>
2024-04-02 13:28:58 +0300

    feat: setting data on react query catch manually

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,13 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { login as loginApi } from '../../services/apiAuth';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
 function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
+
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: () => {
+    onSuccess: (user) => {
+      queryClient.setQueryData(['user'], user);
       navigate('/dashboard');
     },
     onError: (err) => {
--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -18,8 +18,6 @@ export async function getCurrentUser() {
 
   const { data, error } = await supabase.auth.getUser();
 
-  console.log(data);
-
   if (error) throw new Error(error.message);
 
   return data?.user;
