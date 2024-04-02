d20fc8322cbf60f29886a0fe3e6ef5f0f78eb683
BaGorK <edmealemkassahun@gmail.com>
2024-04-02 12:30:40 +0300

    feat: authentication | user login with supabase

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,13 +3,20 @@ import Button from '../../ui/Button';
 import Form from '../../ui/Form';
 import Input from '../../ui/Input';
 import FormRowVertical from '../../ui/FormRowVertical';
+import useLogin from './useLogin';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('edmealem@test.com');
+  const [password, setPassword] = useState('test1234');
+
+  const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!email || !password) return;
+
+    login({ email, password });
   }
 
   return (
@@ -22,8 +29,10 @@ function LoginForm() {
           autoComplete='username'
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
+
       <FormRowVertical label='Password'>
         <Input
           type='password'
@@ -31,10 +40,13 @@ function LoginForm() {
           autoComplete='current-password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          disabled={isLoading}
         />
       </FormRowVertical>
       <FormRowVertical>
-        <Button size='large'>Login</Button>
+        <Button size='large' disabled={isLoading}>
+          Login
+        </Button>
       </FormRowVertical>
     </Form>
   );
