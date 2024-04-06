f0787c24bcb23a736547900d5250486ea9a4a351
BaGorK <edmealemkassahun@gmail.com>
2024-04-06 09:49:53 +0300

    feat: building the app header

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+import ButtonIcon from './ButtonIcon';
+import { HiOutlineUser } from 'react-icons/hi2';
+import Logout from '../features/authentication/Logout';
+import { useNavigate } from 'react-router-dom';
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 4.2rem;
+`;
+
+function HeaderMenu() {
+  const navigate = useNavigate();
+
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate('/account')}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <Logout />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
