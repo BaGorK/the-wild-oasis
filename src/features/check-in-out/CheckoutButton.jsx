e838998f42f3fac6669a07741c2b064e0d74eb40
BaGorK <edmealemkassahun@gmail.com>
2024-04-06 16:06:19 +0300

    feat: implementing checkins and checkouts from the dashboard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,8 +1,17 @@
-import Button from "../../ui/Button";
+/* eslint-disable react/prop-types */
+import Button from '../../ui/Button';
+import { useCheckout } from './useCheckout';
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
+    <Button
+      variation='primary'
+      size='small'
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+    >
       Check out
     </Button>
   );
--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -2,6 +2,9 @@
 import styled from 'styled-components';
 import Tag from '../../ui/Tag';
 import { Flag } from '../../ui/Flag';
+import Button from '../../ui/Button';
+import { Link } from 'react-router-dom';
+import CheckoutButton from './CheckoutButton';
 
 const StyledTodayItem = styled.li`
   display: grid;
@@ -29,11 +32,20 @@ function TodayItem({ activity }) {
     <StyledTodayItem>
       {status === 'unconfirmed' && <Tag type='green'>Arriving</Tag>}
       {status === 'checked-in' && <Tag type='blue'>Departing</Tag>}
-
       <Flag src={guests.countryFlag} alt={`Flag of ${guests.country}`} />
-
       <Guest>{guests.fullName}</Guest>
       <div>{numNights} nights</div>
+      {status === 'unconfirmed' && (
+        <Button
+          size='small'
+          variation='primary'
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+      {status === 'checked-in' && <CheckoutButton bookingId={id} />}
     </StyledTodayItem>
   );
 }
