8651aa802587f027b2a16801a9fa9f8296a2833d
BaGorK <edmealemkassahun@gmail.com>
2024-04-06 12:11:28 +0300

    feat: implement the dark mode when icon btn click

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react/prop-types */
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useEffect } from 'react';
 import { useLocalStorageState } from '../hooks/useLocalStorageState';
 
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, 'isDarkMode');
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add('dark-mode');
+      document.documentElement.classList.remove('light-mode');
+    } else {
+      document.documentElement.classList.add('light-mode');
+      document.documentElement.classList.remove('dark-mode');
+    }
+  }, [isDarkMode]);
 
   function toggleDarkMode() {
     setIsDarkMode((isDark) => !isDark);
