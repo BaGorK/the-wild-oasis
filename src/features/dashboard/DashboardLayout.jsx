2312e4cbb3b77b2d19d4420f541691457c245be7
BaGorK <edmealemkassahun@gmail.com>
2024-04-06 15:11:04 +0300

    feat: displaying basic pie chart using recharts library

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import { useRecentStays } from './useRecentStays';
 import Stats from './Stats';
 import { useCabins } from '../cabins/useCabins';
 import SalesChart from './SalesChart';
+import DurationChart from './DurationChart';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -36,7 +37,7 @@ function DashboardLayout() {
         cabinCount={cabins.length}
       />
       <div>Today activity</div>
-      <div>Chart stay durations</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -44,8 +44,6 @@ function SalesChart({ bookings, numDays }) {
     };
   });
 
-  console.log(data);
-
   const colors = isDarkMode
     ? {
         totalSales: { stroke: '#4f46e5', fill: '#4f46e5' },
@@ -62,7 +60,10 @@ function SalesChart({ bookings, numDays }) {
 
   return (
     <StyledSalesChart>
-      <Heading as='h2'>Sales</Heading>
+      <Heading as='h2'>
+        Sales from {format(allDates.at(0), 'MMM dd yyyy')} &mdash;{' '}
+        {format(allDates.at(-1), 'MMM dd yyyy')}
+      </Heading>
 
       <ResponsiveContainer height={300} width='100%'>
         <AreaChart data={data}>
