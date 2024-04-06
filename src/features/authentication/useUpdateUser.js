e9ea995926b1cf1ea13c1927ab43c8a9ae8329e6
BaGorK <edmealemkassahun@gmail.com>
2024-04-06 11:20:43 +0300

    feat: implement update user data

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,24 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import toast from 'react-hot-toast';
+
+import { updateCurrentUser } from '../../services/apiAuth';
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+    mutationFn: updateCurrentUser,
+    onSuccess: ({ user }) => {
+      toast.success('user account updated successfully!');
+      queryClient.setQueryData(['user'], user);
+
+      queryClient.invalidateQueries({
+        queryKey: ['cabin'],
+      });
+    },
+
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateUser, isUpdating };
+}
