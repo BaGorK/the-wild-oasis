a31bc811ecd89c7661b4e36167ea1df99848b3d6
BaGorK <edmealemkassahun@gmail.com>
2024-04-07 14:19:33 +0300

    Update LoginForm with test credentials

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import useLogin from './useLogin';
 import SpinnerMini from '../../ui/SpinnerMini';
 
 function LoginForm() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('test@test.com');
+  const [password, setPassword] = useState('test1234');
 
   const { login, isLoading } = useLogin();
 
